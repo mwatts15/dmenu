94938cfc0e855f60d12314881e0940ec7b1a2ae8
Hiltjo Posthuma <hiltjo@codemadness.org>
2016-06-17 15:33:50 +0200

    incremental mode: dmenu outputs text each time a key is pressed.

--- dmenu.c
+++ dmenu.c
@@ -436,6 +436,10 @@ keypress(XKeyEvent *ev)
 		match();
 		break;
 	}
+	if (incremental) {
+		puts(text);
+		fflush(stdout);
+	}
 	drawmenu();
 }
 
@@ -601,7 +605,7 @@ static void
 usage(void)
 {
 	fputs("usage: dmenu [-b] [-f] [-i] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-v]\n", stderr);
+	      "             [-nb color] [-nf color] [-r] [-sb color] [-sf color] [-v]\n", stderr);
 	exit(1);
 }
 
@@ -619,6 +623,8 @@ main(int argc, char *argv[])
 			topbar = 0;
 		else if (!strcmp(argv[i], "-f"))   /* grabs keyboard before reading stdin */
 			fast = 1;
+		else if (!strcmp(argv[i], "-r"))   /* incremental */
+			incremental = 1;
 		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
